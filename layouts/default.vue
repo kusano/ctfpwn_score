270c3b9ab8fecb68c2b3b7b64cd2a93d4279d595
kusano <kusano@users.noreply.github.com>
2019-08-04 05:35:58 +0900

    解答履歴の削除機能を実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,10 +11,46 @@
       </v-container>
     </v-content>
     <v-footer app absolute>
+      <div>
+        <a href="https://twitter.com/kusano_k" target="_blank">@kusano_k</a>
+      </div>
       <v-spacer />
       <div>
-        <a href="https://twitter.com/kusano_k">@kusano_k</a>
+        <v-btn x-small>
+          <v-icon color="error" @click="dialogRemove=true">mdi-eraser</v-icon>
+        </v-btn>
       </div>
     </v-footer>
+
+    <v-dialog v-model="dialogRemove" max-width="480">
+      <v-card>
+        <v-card-title>解答履歴削除</v-card-title>
+        <v-divider />
+        <v-card-text>解答履歴を削除して、全ての問題を未解答の状態に戻しますか？</v-card-text>
+        <v-divider />
+        <v-card-actions>
+          <v-spacer />
+          <v-btn @click="removeHistory()" color="error">削除</v-btn>
+          <v-btn @click="dialogRemove=false">キャンセル</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </v-app>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialogRemove: false,
+      }
+    },
+    methods: {
+      removeHistory() {
+        localStorage.removeItem("ctfpwn");
+        location.reload();
+      }
+    }
+  }
+</script>
