51d7b0e90d4ef8bb2a25c7083623d976d0dbe7ba
kusano <kusano@users.noreply.github.com>
2019-08-04 03:00:57 +0900

    画面のデザインを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,55 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div v-if="problems.length==0">
+    <v-progress-circular
+      color="primary"
+      size="128"
+      indeterminate
+    />
+  </div>
+  <div v-else>
+    <v-layout wrap>
+      <v-flex v-for="(problem, index) in problems" xs12 sm6 md4 lg3 xl2>
+        <v-card class="mx-2 my-4">
+          <v-card-title>
+            {{problem.title}}
+            <div v-for="g in problem.genre">
+              <span class="mx-1" />
+              <v-chip small>{{g}}</v-chip>
+            </div>
+          </v-card-title>
+          <v-img :src="problem.image" style="filter:grayscale(90%)">
+          </v-img>
+          <v-card-text v-html="problem.description" />
+          <v-divider />
+          <v-card-actions>
+            <v-text-field label="Flag" placeholder="FLAG{????}">
+              <template v-slot:append-outer>
+                <v-btn>
+                  <v-icon left>mdi-send</v-icon>
+                  Submit
+                </v-btn>
+              </template>
+            </v-text-field>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      problems: []
+    }
+  },
+  created() {
+    fetch("/files/problem.json")
+      .then(res => res.json())
+      .then(data => {
+        this.problems = data.problems;
+      });
   }
 }
 </script>
