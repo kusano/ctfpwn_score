f40d77060b5b5b819285221d0ce5d7d74094c1f2
kusano <kusano@users.noreply.github.com>
2019-08-04 05:45:33 +0900

    ページタイトルを変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: 'Malleus CTF Pwn Challenge',
+    title: 'Malleus CTF Pwn Challenge',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
